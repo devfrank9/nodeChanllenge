89d0c2001aad19eb953f72db52461ce3f42a36d2
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-18 02:18:35 +0900

    챌린지 성공

--- assignment08/movieController.js
+++ assignment08/movieController.js
@@ -5,6 +5,31 @@ import {
   getMovieByMinimumYear,
 } from "./db";
 
-export const home = (req, res) => {};
-export const movieDetail = (req, res) => {};
-export const filterMovie = (req, res) => {};
+export const home = (req, res) => {
+  const videos = getMovies();
+  return res.render("home", { pageTitle: "Videos!", videos });
+};
+export const movieDetail = (req, res) => {
+  const { id } = req.params;
+  const video = getMovieById(id);
+  console.log(video);
+  return res.render("detail", { video });
+};
+export const filterMovie = (req, res) => {
+  let videos = getMovies();
+  const { year, rating } = req.query;
+  if (year) {
+    videos = getMovieByMinimumYear(year);
+    return res.render("search", {
+      message: `Searching by year: ${year}`,
+      videos,
+    });
+  }
+  if (rating) {
+    videos = getMovieByMinimumRating(rating);
+    return res.render("search", {
+      message: `Searching by rate: ${rating}`,
+      videos,
+    });
+  }
+};
