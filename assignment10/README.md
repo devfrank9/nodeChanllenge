f9803beb67f8ecde49105e087963d03ba042fc94
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-19 10:06:34 +0900

    과제 해결 완료

---
+++ assignment10/README.md
@@ -0,0 +1,9 @@
+# 위튜브 온라인 부트캠프 10일차 챌린지
+
+- 오늘은 금요일 입니다. 오늘의 과제는 매우 짧습니다!
+
+- 오늘은 Movie 모델을 완성해주세요
+- models/Movie.js 파일에 작성하면 됩니다.
+- Movie 모델은 title,summary,year,rating,genres 를 갖고 있습니다.
+- genres 는 반드시 string 배열 타입이어야 합니다.
+- 모든 필드들은 필수조건(required) 입니다.
--- assignment10/src/models/Movie.js
+++ assignment10/src/models/Movie.js
@@ -1,3 +1,15 @@
 import mongoose from "mongoose";
 
 // Create a Movie Model here.
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  year: { type: String, required: true },
+  rating: { type: String, required: true },
+  genres: [{ type: String, required: true }],
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+export default Movie;
