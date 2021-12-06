21d79c417e9003c4ad75753d1674b713e667bcc0
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-06 21:12:13 +0900

    챌린지 성공

--- assignment18/src/index.js
+++ assignment18/src/index.js
@@ -4,6 +4,13 @@ const psBtn = videoController.querySelector("#playPauseBtn");
 const volumeBtn = videoController.querySelector("#volume");
 const volumeRange = videoController.querySelector("#volumeRange");
 
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
+const videoContainer = document.getElementById("videoContainer");
+
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -45,6 +52,60 @@ const handleVolume = (event) => {
   video.volume = volumeValue = value;
 };
 
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substr(14, 5);
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currenTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenIcon.classList = "fas fa-expand";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenIcon.classList = "fas fa-compress";
+  }
+};
+
 psBtn.addEventListener("click", handlePlayAndStop);
 volumeBtn.addEventListener("click", handleSound);
 volumeRange.addEventListener("input", handleVolume);
+timeline.addEventListener("input", handleTimelineChange);
+video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+fullScreenBtn.addEventListener("click", handleFullScreen);
+document.addEventListener("keydown", (event) => {
+  const fullscreen = document.fullscreenElement;
+  if (event.keyCode === 70 && !fullscreen) {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+  if (event.keyCode === 27 && fullscreen) {
+    videoContainer.exitFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+  if (event.keyCode === 32 && video.paused) {
+    video.play();
+    psBtn.className = "fas fa-pause";
+  } else if (event.keyCode === 32 && !video.paused) {
+    video.pause();
+    psBtn.className = "fas fa-play";
+  }
+});
